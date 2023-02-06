123386cb304879c2ce40eda8feb079227728018d
Ali AYTEKİN <96261457+aytek55@users.noreply.github.com>
2023-02-06 14:52:56 +0300

    dash

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/tutorial/intro">
+            to="/docs/dashboard/dashboard">
             ShopiVerse öğrenin - 5 dk ⏱️
           </Link>
         </div>
