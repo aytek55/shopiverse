59c36d15eed6b0f1760929b7611222f40761a8c0
Ali AYTEKİN <96261457+aytek55@users.noreply.github.com>
2023-02-09 14:59:12 +0300

    end

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: 'img/shopiverseicon.jpg',
 
   // Set the production url of your site here
-  url: 'http://docs.shopiverse.com/',
+  url: 'https://github.com/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/shopiverse/',
