453f20ed6d0939a0613e93d5f6ef4c94b81ff9be
Ali AYTEKİN <96261457+aytek55@users.noreply.github.com>
2023-07-12 18:08:53 +0300

    cpnael

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,11 +11,12 @@ const config = {
   favicon: 'img/shopiverseicon.jpg',
 
   // Set the production url of your site here
-  url: 'https://github.com/',
+  url: 'https://github.com/', //github
+  //url: 'https://docs.shopiverse.com/',  //cpanel
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/shopiverse/', //Github
-  //baseUrl: '', //Cpanel 
+  //baseUrl: '/', //Cpanel 
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
